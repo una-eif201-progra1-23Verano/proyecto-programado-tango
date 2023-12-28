5013ba70df2d6f1302beeb861524fc39b973a95b
Brryan12 <153683809+Brryan12@users.noreply.github.com>
2023-12-27 19:42:31 -0600

    Update Menu.h

--- include/Menu.h
+++ include/Menu.h
@@ -1,1 +1,26 @@
+#include <string>
 
+/*!
+ * \class Menu
+ * \brief The Menu class represents a menu.
+ */
+
+class Menu {
+public:
+    Menu();
+    ~Menu();
+    void displayMenu() const;
+    void handleUserInput();
+
+private:
+    struct Node {
+        std::string option;
+        Node* next;
+    };
+
+    Node* head;
+
+    void insertOption(const std::string& opt);
+    static void handleOption(const std::string& opt);
+    void clear();
+};
