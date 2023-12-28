3208a75087cc7b2cc894de3f9869cc15bc163954
IanZuniga25 <153691403+IanZuniga25@users.noreply.github.com>
2023-12-27 19:21:06 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,6 @@
 #include <iostream>
 #include <cstdlib>
 
- *
- * \section references References
- *
- * <a href="https://github.com/una-eif201-progra1-master/dev.funda.oop-inheritance">Github repository of the example</a>
- *
- * \date 2021-11-23
- * \author Maikol Guzman Alan
- * \copyright <a href="https://mikeguzman.dev/">mikeguzman.dev</a>
- *
- * \file main.cpp
- * \brief The main.cpp file contains the entry point of the program.
- */
-
 int main() {
     system("clear");
     std::cout << "Universidad Nacional de Costa Rica - www.mikeguzman.dev" << std::endl;
