cd33c9cfa25124e7ad71861ea4693db363248deb
brryan12 <bryancr1208@gmail.com>
2024-01-09 17:46:13 -0600

    intento

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@ int main() {
     menu.handleUserInput();
 */
     Student student1(15, "Bryan", "Informatica");
-    std::cout << "ID: " << student1.getId() << ", Name: " << student1.getName() << ", Carrera: " << student1.getMajor() << std::endl;
+    std::cout << student1.toString();
     Courses course1("Calculo",100,"4184");
-    cout<<course1.getCourseName()<<" "<<course1.getGrades()<<" "<<course1.getCourseCode();
+    cout<<course1.toString();
     return 0;
 }
