13d058f6d67789b1e71fab479052785b7a272bd2
Brryan12 <bryancr1208@gmail.com>
2024-01-10 13:15:09 -0600

    person

---
+++ Person.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ender on 10/1/2024.
+//
+
+#include "Person.h"
---
+++ Person.h
@@ -0,0 +1,14 @@
+//
+// Created by Ender on 10/1/2024.
+//
+
+#ifndef MAIN_PERSON_H
+#define MAIN_PERSON_H
+
+
+class Person {
+
+};
+
+
+#endif //MAIN_PERSON_H
