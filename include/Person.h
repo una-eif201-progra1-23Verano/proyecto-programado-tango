532904efb2d6f90add673f87989d32f3e9d6ec58
Brryan12 <bryancr1208@gmail.com>
2024-01-10 13:16:30 -0600

    person

--- include/Person.h
+++ include/Person.h
@@ -4,10 +4,15 @@
 
 #ifndef MAIN_PERSON_H
 #define MAIN_PERSON_H
+#include <sstream>
+#include <iostream>
 
-
+using namespace std;
 class Person {
-
+private:
+    int id;
+    string name;
+public:
 };
 
 
