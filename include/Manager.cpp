8a2ca8428103c6db6158375afc5a5ce5a1c38531
IanZuniga25 <ian.zuniga.alvarez@est.una.ac.cr>
2024-02-01 18:11:03 -0600

    Nuevo manager

---
+++ include/Manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ennia on 1/2/2024.
+//
+
+#include "Manager.h"
---
+++ include/Manager.h
@@ -0,0 +1,14 @@
+//
+// Created by Ennia on 1/2/2024.
+//
+
+#ifndef MAIN_MANAGER_H
+#define MAIN_MANAGER_H
+
+
+class Manager {
+
+};
+
+
+#endif //MAIN_MANAGER_H
