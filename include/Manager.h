7559973cb13d38c41ed771970482fdaef6428b2d
IanZuniga25 <ian.zuniga.alvarez@est.una.ac.cr>
2024-02-07 20:06:33 -0600

    Update Manager.h

--- include/Manager.h
+++ include/Manager.h
@@ -2,7 +2,8 @@
 #ifndef MAIN_MANAGER_H
 #define MAIN_MANAGER_H
 #include<fstream>
-
+#include "iostream"
+using namespace std;
 class Manager {
     std::ofstream archivo;
     archivo.open(nombreArchivo.c_str(),ios::out);
